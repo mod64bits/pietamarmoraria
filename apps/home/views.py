775c56b3172b0c8491f105f7f822c3efbd1b669d
mod64bits <mod64bits@gmail.com>
2020-10-07 15:49:34 -0300

    configuracao banco de dados

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,6 +1,15 @@
+from django.contrib.messages.views import SuccessMessageMixin
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, UpdateView
+from .models import InformacoesSite
 
 
 class Home(TemplateView):
     template_name = 'home/home.html'
+
+
+class EditarInformacoesDoSite(SuccessMessageMixin, UpdateView):
+    model = InformacoesSite
+    fields = '__all__'
+    template_name = 'home/EditInfoSite.html'
+    success_message = "Informações do Site Atualizados com Sucesso"
