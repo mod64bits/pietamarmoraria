d4977f2c38d2ca3547e6794638e3eed47d01569c
mod64bits <mod64bits@gmail.com>
2020-11-09 10:22:36 -0300

    condiguracoes

--- pietamarmoraria/settings.py
+++ pietamarmoraria/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'pietamarmoraria.ind.br']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'pietamarmoraria.ind.br', 'agmarmoraria.com.br']
 
 AUTH_USER_MODEL = 'usuarios.User'
 
