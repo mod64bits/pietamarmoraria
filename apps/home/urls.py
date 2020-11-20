3e26fb9294bb698647e6906074082074d4a25ef8
mod64bits <mod64bits@gmail.com>
2020-11-19 21:38:25 -0300

    final ajuste servicos

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from apps.home.views import Home, EditarInformacoesDoSite
 from apps.servicos.views import TodosServicos, categoria
-from .views import ContatoSite, ProjetosList, ProjetoDetalhe
+from .views import ContatoSite, ProjetosList, ProjetoDetalhe, ServicoDetalhe
 
 
 app_name = 'home'
@@ -10,6 +10,7 @@ urlpatterns = [
     path('', Home.as_view(), name='home'),
     path('projetos/', ProjetosList.as_view(), name='projetos'),
     path('projetos/<slug:slug>/', ProjetoDetalhe.as_view(), name='projetos_detalhe'),
+    path('servicos/<slug:slug>/', ServicoDetalhe.as_view(), name='servico_detalhe'),
     path('contato/', ContatoSite.as_view(), name='contato'),
     path('servicos/', TodosServicos.as_view(), name='todos_servicos'),
     path('categoria/<str:nome>/', categoria, name='categoria_servico'),
