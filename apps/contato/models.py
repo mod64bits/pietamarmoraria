3e03d89f2354574588f6808d399cd3847e3eb287
mod64bits <mod64bits@gmail.com>
2020-11-27 14:46:21 -0300

    ajuste Falha Contato

--- apps/contato/models.py
+++ apps/contato/models.py
@@ -1,14 +1,9 @@
 from django.db import models
 from django.urls import reverse
-from django.db.models.signals import post_save
-from .sinais import contato_signal
-
 
 class Contato(models.Model):
     nome = models.CharField('Nome', max_length=100)
     slug = models.SlugField(max_length=255, unique=True, editable=False)
-    slug_field_name = 'slug'
-    slug_from = 'nome'
     email = models.EmailField('Email')
     whatsapp = models.CharField('Whatsapp', max_length=100)
     mensagem = models.TextField('Mensagem')
@@ -25,7 +20,7 @@ class Contato(models.Model):
         return self.nome
 
 
-post_save.connect(contato_signal, dispatch_uid=Contato)
+
 
 
 
