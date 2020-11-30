ccee88f41393791b4b77a91698c4106abc306777
mod64bits <mod64bits@gmail.com>
2020-11-30 09:07:07 -0300

    ajuste Falha Contato

--- apps/contato/models.py
+++ apps/contato/models.py
@@ -1,26 +1,22 @@
 from django.db import models
 from django.urls import reverse
 
+
 class Contato(models.Model):
     nome = models.CharField('Nome', max_length=100)
-    slug = models.SlugField(max_length=255, unique=True, editable=False)
+    slug = models.SlugField(max_length=255, unique=True, editable=False, null=True, blank=True)
     email = models.EmailField('Email')
     whatsapp = models.CharField('Whatsapp', max_length=100)
-    mensagem = models.TextField('Mensagem')
+    menssagem = models.TextField('Mensagem')
     respondida = models.BooleanField('Respondido?', default=False)
     created = models.DateTimeField('Criado em', auto_now_add=True)
     modified = models.DateTimeField('Modificado em', auto_now=True)
-
-
 
     def get_absolute_url(self):
         return reverse('home:contato')
 
     def __str__(self):
         return self.nome
-
-
-
 
 
 
--- apps/portifolio/models.py
+++ apps/portifolio/models.py
@@ -25,7 +25,7 @@ class Projeto(models.Model):
     sobre = models.TextField('Sobre ', help_text='Sobre o projeto', null=True, blank=True)
     categoria_projeto = models.ForeignKey(Categoria, name="Categoria", on_delete=models.PROTECT)
     home = models.BooleanField('Mostrar na Home?', default=True)
-    imagem_de_capa = models.ImageField('Imagem de Capa', upload_to='projetos_img/%Y/%m/%d/')
+    imagem_de_capa = models.ImageField('Imagem de Capa', upload_to='projetos')
     slug = models.SlugField(max_length=255, unique=True, editable=False)
     slug_field_name = 'slug'
     slug_from = 'nome'
@@ -51,7 +51,7 @@ def renomear_imagem(instance, filename):
 class Imagem(models.Model):
     nome = models.CharField('Nome', max_length=50)
     imagem = models.ImageField('Imagem', upload_to=renomear_imagem)
-    projeto_portifolio = models.ForeignKey(Projeto, name='Projeto', on_delete=models.PROTECT)
+    projeto_portifolio = models.ForeignKey(Projeto, name='Projeto', on_delete=models.CASCADE)
     created = models.DateTimeField('Criado em', auto_now_add=True)
     modified = models.DateTimeField('Modificado em', auto_now=True)
 
